c355f2961562fc927742403999407e0096888a80
unknown <you@example.com>
2025-01-17 11:16:40 +0600

    navbar commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,22 +1,21 @@
-import { Link } from 'react-router-dom';
+import {  NavLink } from 'react-router-dom';
 import logo from '../assets/Logo.png'
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='p-6'>
             <div className='flex items-center justify-between'>
-                <div><img src={logo} alt="" /></div>
-                <div><button>sort by view</button></div>
-                <div><button>Blog</button></div>
-                <div></div>
+                <img  src={logo} alt="" />
+                <button className='bg-slate-200   font-semibold btn'>sort by view</button>
+                <button className='bg-red-500 text-white font-semibold btn' >Blog</button> 
             </div>
             <div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><Link to={'/'}>All</Link></li>
-                        <li><Link to={'/'}>All</Link></li>
-                        <li><Link to={'/'}>All</Link></li>
-                        <li><Link to={'/'}>All</Link></li>
+                <div className="navbar-center hidden lg:flex justify-center">
+                    <ul className="menu menu-horizontal px-1 gap-5">
+                        <li><NavLink className={({isActive})=> isActive ? 'bg-red-500 text-white font-semibold' : 'bg-slate-300'} to={'/'} >All</NavLink></li>
+                        <li><NavLink className={({isActive})=> isActive ? 'bg-red-500 text-white font-semibold' : 'bg-slate-300'} to={'/music'}>Music</NavLink></li>
+                        <li><NavLink className={({isActive})=> isActive ? 'bg-red-500 text-white font-semibold' : 'bg-slate-300'} to={'/comedy'}>Comedy</NavLink></li>
+                        <li><NavLink className={({isActive})=> isActive ? 'bg-red-500 text-white font-semibold' : 'bg-slate-300'} to={'/drawing'}>Drawing</NavLink></li>
                     </ul>
                 </div>
             </div>
